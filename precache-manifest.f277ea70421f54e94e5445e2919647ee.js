96a9f623eb79a21ec3ed9fbae06f3e2482b96584
Firman Taruna Nugraha <firmantr3@gmail.com>
2019-02-18 10:21:26 +0700

    update 1.0.1

---
+++ precache-manifest.f277ea70421f54e94e5445e2919647ee.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0234f374c4c2e4497ef",
+    "url": "/ftn-vue-pwa-demo/css/app.aa4e6d09.css"
+  },
+  {
+    "revision": "b0234f374c4c2e4497ef",
+    "url": "/ftn-vue-pwa-demo/js/app.b6cb1f74.js"
+  },
+  {
+    "revision": "124c0f2c1e1cd33a8b35",
+    "url": "/ftn-vue-pwa-demo/css/chunk-2c4a5ecb.9eb8e684.css"
+  },
+  {
+    "revision": "124c0f2c1e1cd33a8b35",
+    "url": "/ftn-vue-pwa-demo/js/chunk-2c4a5ecb.6bb14850.js"
+  },
+  {
+    "revision": "7cd46873b05278dd120f",
+    "url": "/ftn-vue-pwa-demo/js/chunk-2d0abcb4.f76e54dd.js"
+  },
+  {
+    "revision": "3574e8eaf0c6c48c9c27",
+    "url": "/ftn-vue-pwa-demo/js/chunk-2d213127.25f2a0e3.js"
+  },
+  {
+    "revision": "935a0939691a324fc57d",
+    "url": "/ftn-vue-pwa-demo/js/chunk-2d21eb45.82f53280.js"
+  },
+  {
+    "revision": "cbd8aa8657afbb5d9c22",
+    "url": "/ftn-vue-pwa-demo/js/chunk-2d225216.e134e757.js"
+  },
+  {
+    "revision": "bfd3d6c653946e56b068",
+    "url": "/ftn-vue-pwa-demo/js/chunk-2d23118b.873f346c.js"
+  },
+  {
+    "revision": "35813fa38b29173928e7",
+    "url": "/ftn-vue-pwa-demo/js/chunk-3beb6d94.199c55c0.js"
+  },
+  {
+    "revision": "490889c138007770ad88",
+    "url": "/ftn-vue-pwa-demo/js/chunk-7459534f.f35252c7.js"
+  },
+  {
+    "revision": "95e89ea8a02a683fc930",
+    "url": "/ftn-vue-pwa-demo/css/chunk-93c5fdaa.2a2e351b.css"
+  },
+  {
+    "revision": "95e89ea8a02a683fc930",
+    "url": "/ftn-vue-pwa-demo/js/chunk-93c5fdaa.69464639.js"
+  },
+  {
+    "revision": "a32e6714a90f15b448a3",
+    "url": "/ftn-vue-pwa-demo/js/chunk-da4791d4.9b6d7b99.js"
+  },
+  {
+    "revision": "838ab693ec76e4d759d0",
+    "url": "/ftn-vue-pwa-demo/css/chunk-vendors.99dfb37c.css"
+  },
+  {
+    "revision": "838ab693ec76e4d759d0",
+    "url": "/ftn-vue-pwa-demo/js/chunk-vendors.67abc336.js"
+  },
+  {
+    "revision": "3c2fcb9e2b6513b8ec02",
+    "url": "/ftn-vue-pwa-demo/js/lang-en.305289b1.js"
+  },
+  {
+    "revision": "24e3fd2ff10aebaa96ac",
+    "url": "/ftn-vue-pwa-demo/js/lang-id.6a7f8577.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ftn-vue-pwa-demo/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ftn-vue-pwa-demo/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ftn-vue-pwa-demo/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ftn-vue-pwa-demo/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ftn-vue-pwa-demo/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "07bd1587dff1a8ab316c1b8d36528147",
+    "url": "/ftn-vue-pwa-demo/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ftn-vue-pwa-demo/robots.txt"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/ftn-vue-pwa-demo/precache-manifest.056524907c82304dd8970f9ff4893261.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/ftn-vue-pwa-demo/precache-manifest.f277ea70421f54e94e5445e2919647ee.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
